6c907a37317fd3c95caf2b84973af1507bed2e7f
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-11-30 10:16:58 +0000

    connected supabase, created cabins api req

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import Account from "./pages/Account";
 import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
@@ -21,6 +22,7 @@ const App = () => {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="account" element={<Account />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
             <Route path="settings" element={<Settings />} />
             <Route path="users" element={<Users />} />
           </Route>
