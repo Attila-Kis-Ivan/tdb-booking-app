ef13dd589c02c1e986f6dc35120cfa07a1273abb
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-12-10 09:48:12 +0000

    created useBooking and booking. fetch data from supabase and display it

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
 import { format, isToday } from "date-fns";
+import { Cabin, Stacked, Amount } from "./BookingRow-styles";
 
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
@@ -7,34 +7,7 @@ import Table from "../../ui/Table";
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 
-const Cabin = styled.div`
-  font-size: 1.6rem;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  font-family: "Sono";
-`;
-
-const Stacked = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.2rem;
-
-  & span:first-child {
-    font-weight: 500;
-  }
-
-  & span:last-child {
-    color: var(--color-grey-500);
-    font-size: 1.2rem;
-  }
-`;
-
-const Amount = styled.div`
-  font-family: "Sono";
-  font-weight: 500;
-`;
-
-function BookingRow({
+export const BookingRow = ({
   booking: {
     id: bookingId,
     created_at,
@@ -47,7 +20,7 @@ function BookingRow({
     guests: { fullName: guestName, email },
     cabins: { name: cabinName },
   },
-}) {
+}) => {
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -81,6 +54,6 @@ function BookingRow({
       <Amount>{formatCurrency(totalPrice)}</Amount>
     </Table.Row>
   );
-}
+};
 
 export default BookingRow;
