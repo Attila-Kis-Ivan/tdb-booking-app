4e7d0bebf616d7477a6cd4157e0b5cc91c7ecaa9
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-12-13 15:58:00 +0000

    created authenticatin with supabase

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
-import Button from "../../ui/Button";
+import Button from "../../ui/Button-Styles";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { login } from "../../services/apiAuth";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("ati@test.com");
+  const [password, setPassword] = useState("password007");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login({ email, password });
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -16,7 +25,7 @@ function LoginForm() {
         <Input
           type="email"
           id="email"
-          // This makes this form better for password managers
+          // for password managers
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -29,10 +38,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? "Log in" : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,16 @@
+import LoginForm from "../features/authentication/LoginForm";
+import Heading from "../ui/Heading";
+import Logo from "../ui/Logo";
 import { LoginLayout } from "./Login-styles";
 
 const Login = () => {
-  return <LoginLayout>Login</LoginLayout>;
+  return (
+    <LoginLayout>
+      <Logo />
+      <Heading as="h4">Log in to your account</Heading>
+      <LoginForm />
+    </LoginLayout>
+  );
 };
 
 export default Login;
